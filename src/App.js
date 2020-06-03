f831ab770055f999a9ca452a3026f5e900d5fbbe
3jinneeya <yujin9301@gmail.com>
2020-06-03 20:38:59 +0900

    prop-types

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food(props) {
-return <h1>I like {props.fav}</h1>;
-}
 /**
  * 방법 1
 function Food(props) {
@@ -18,19 +16,63 @@ return <h1>I like {fav}</h1>;
 }
 
 */
-
-
-function App() {
-//  return <div className="App" />;
-  return (
-    <div>
-      <h1>hello</h1>
-      <Food fav="kimchi"></Food>
-      <Food fav="ramen"></Food>
-      <Food fav="samgiopsal"></Food>
-      <Food fav="chukumi"></Food>
-    </div>
+function Food({ name, picture, rating}) {
+return (
+  <div>
+    <h1>I like {name}</h1>
+<h4>{rating}/5.0</h4>
+    <img src={picture} alt={name} ></img>
+  </div>
   );
 }
 
+const foodILike = [
+  {
+    id: 1,
+    name: 'kimchi',
+    image:'https://m.hansanggung.com/web/product/big/hansanggung1_44.jpg',
+    rating: 5
+  },
+  {
+    id: 2,
+    name: 'ramen',
+    image:'https://post-phinf.pstatic.net/MjAxODAyMDVfNDAg/MDAxNTE3NzkzMzYwNjQz.fqaN8BhckNlQsH-W6I8OIy2ZsA76pAhkVjtFNa0TEw8g.G8FVDa92ISHCm0kEcJHupls7ObpE9IGIYKXw9SFgUBAg.JPEG/image_6921511681517793230752.jpg?type=w1200',
+    rating: 4
+  },
+  {
+    id: 3,
+    name: 'samgiopsal',
+    image:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRm1p6Om8NS0rreCe9jyFODbaMg5YJZj_GVzFklK9aNZ5E6qoo_&usqp=CAU',
+    rating: 2
+  },
+  {
+    id: 4,
+    name: 'chukumi',
+    image:'https://recipe1.ezmember.co.kr/cache/recipe/2019/03/05/52d2be99c015378a75c9db81362422c71.jpg',
+    rating: 4.9
+  },
+];
+
+//npm install prop-types
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+}
+
+//function renderFood(dish){
+//  return <Food name={dish.nme} picture={dish.image} />;
+//}
+// const renderFood = dish => <Food name={dish.nme} picture={dish.image} />
+function App() {
+//  return <div className="App" />;
+//  return (<div> {foodILike.map(renderFood)} </div> );
+return (
+<div>
+  {foodILike.map(dish => (
+    <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
+  ))}
+</div>
+);
+}
 export default App;
