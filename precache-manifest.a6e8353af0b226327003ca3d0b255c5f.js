0c195b5d2ad54752faf0d20a83df6d51f06b451e
3jinneeya <yujin9301@gmail.com>
2020-06-17 22:37:06 +0900

    Updates

---
+++ precache-manifest.a6e8353af0b226327003ca3d0b255c5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f55c3229e2adf721720a55d1192107",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "3c3e3ee29ad2697e27cf",
+    "url": "/movie_app_2020/static/css/main.2fb21aa4.chunk.css"
+  },
+  {
+    "revision": "b17fb38856ffb36d44ef",
+    "url": "/movie_app_2020/static/js/2.14c54020.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.14c54020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3e3ee29ad2697e27cf",
+    "url": "/movie_app_2020/static/js/main.a6bb50eb.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
